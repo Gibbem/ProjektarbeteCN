aa90d7cee09b5b18dbbdd67a8e38b5ae1ed8b55f
Gibbem <31651311+Gibbem@users.noreply.github.com>
2021-09-10 22:50:23 +0200

    added js carousel

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,30 @@
 //         navElement.classList.add('show')
 //     }
 // }
+// img/product1-mobile.jpg
+// <img class="slideshow__image" src="img/product1-mobile.jpg" />
+// imgElement == <img />
+
+
+const imgList = [
+    'img/product1-mobile.jpg',
+    'img/product2-mobile.jpg',
+    'img/product3-mobile.jpg',
+    'img/product4-mobile.jpg',
+    'img/product5-mobile.jpg',
+    'img/product6-mobile.jpg',
+    'img/product7-mobile.jpg',
+    'img/product8-mobile.jpg',
+    'img/product9-mobile.jpg',
+    'img/product10-mobile.jpg'
+]
+
+function placeImage(url) {
+  const sliderElement = document.querySelector('.slideshow-container')
+  const imgElement = document.createElement('img')
+  imgElement.setAttribute('class', 'slideshow__image')
+  imgElement.setAttribute('src', url)
+  sliderElement.append(imgElement)
+}
+
+placeImage('img/product1-mobile.jpg')
