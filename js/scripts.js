83ceb8e5c2098e9920547bab96c7262878c06fb3
Gibbem <31651311+Gibbem@users.noreply.github.com>
2021-09-10 21:29:07 +0200

    rework

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 
 
-// ^Detta har Calle gjort
+// // ^Detta har Calle gjort
 
-function toggleMenu() {
-    const navElement = document.querySelector('nav.navigation-menu')
-    if (navElement.classList.contains('show')) {
-        navElement.classList.remove('show')
-    } else {
-        navElement.classList.add('show')
-    }
-}
+// function toggleMenu() {
+//     const navElement = document.querySelector('nav.navigation-menu')
+//     if (navElement.classList.contains('show')) {
+//         navElement.classList.remove('show')
+//     } else {
+//         navElement.classList.add('show')
+//     }
+// }
